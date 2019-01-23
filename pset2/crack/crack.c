01ed6cb340b9f5304eae635f1ea4a03e2b73638c
mrfrost91 <metallfrost@gmail.com>
2019-01-23 16:50:43 +0300

    Update crack.c

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -1,45 +1,137 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <cs50.h>
 #include <string.h>
-#include <ctype.h>
+#include <cs50.h>
+#include <crypt.h>
 
 int main(int argc, string argv[])
 {
-    if (argc > 2)
+    if (argc != 2 || strlen(argv[1]) != 13)
     {
-        eprintf("program shouldn't have more than one argument\n");
+        printf("Usage: ./crack hash\n");
         return 1;
     }
-    else if (argc == 1)
+    int maxKeyLen = 5;
+    char salt[3] = {argv[1][0], argv[1][1], '\0'};
+    char pass[maxKeyLen];
+    int passLength = strlen(pass);
+    string resultHash = "";
+    char alphabet[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' , 'W', 'X', 'Y', 'Z'};
+    int alphabet_len = strlen(alphabet);
+    long x = 0;
+    // iterate over letters in first digit
+    for (int i = 0; i < alphabet_len; i++)
     {
-        eprintf("program should have one argument as a cipher key\n");
-        return 1;
-    }
-    int key = atoi(argv[1]);
-    string input = get_string("plaintext: ");
-    int input_len = strlen(input);
-    char cipher[input_len + 1];
-    for (int i = 0; i < input_len + 1; i++)
-    {
-        cipher[i] = input[i];
-        if (isalpha(input[i]) && islower(input[i]))
+        pass[0] = alphabet[i];
+        x++;
+        resultHash = crypt(pass, salt);
+
+        if (strcmp(argv[1], resultHash) == 0)
         {
-            cipher[i] = (cipher[i] + key - 97) % 26;
-            cipher[i] += 97;
-            /*input[i] = cipher[i];*/
-        }
-        else if (isalpha(input[i]) && isupper(input[i]))
-        {
-            cipher[i] = (cipher[i] + key - 65) % 26;
-            cipher[i] += 65;
-            /*input[i] = cipher[i];*/
-        }
-        else if (i == input_len)
-        {
-            cipher[i] = '\0';
+            printf("password: %s, %ld iterations\n", pass, x);
+            printf("%s, %s\n", resultHash, argv[1]);
+            return 0;
         }
     }
-    printf("ciphertext: %s\n", cipher);
-    return 0;
+
+    // iterate over first two digits
+    for (int i = 0; i < alphabet_len; i++)
+    {
+        pass[0] = alphabet[i];
+        x++;
+        for (int j = 0; j < alphabet_len; j++)
+        {
+            pass[1] = alphabet[j];
+            x++;
+            resultHash = crypt(pass, salt);
+            if (strcmp(argv[1], resultHash) == 0)
+            {
+                printf("password: %s, %ld iterations\n", pass, x);
+                printf("%s, %s\n", resultHash, argv[1]);
+                return 0;
+            }
+        }
+}
+    // iterate over first three digits
+        for (int i = 0; i < alphabet_len; i++)
+    {
+        pass[0] = alphabet[i];
+        x++;
+        for (int j = 0; j < alphabet_len; j++)
+        {
+            pass[1] = alphabet[j];
+            x++;
+        for (int k = 0; k < alphabet_len; k++)
+        {
+            pass[2] = alphabet[k];
+            x++;
+            resultHash = crypt(pass, salt);
+            if (strcmp(argv[1], resultHash) == 0)
+            {
+                printf("password: %s, %ld iterations\n", pass, x);
+                printf("%s, %s\n", resultHash, argv[1]);
+                return 0;
+            }
+        }
+}
+}
+    // iterate over first four digits
+        for (int i = 0; i < alphabet_len; i++)
+    {
+        pass[0] = alphabet[i];
+        x++;
+        for (int j = 0; j < alphabet_len; j++)
+        {
+            pass[1] = alphabet[j];
+            x++;
+        for (int k = 0; k < alphabet_len; k++)
+        {
+            pass[2] = alphabet[k];
+            x++;
+        for (int l = 0; l < alphabet_len; l++)
+        {
+            pass[3] = alphabet[l];
+            x++;
+            resultHash = crypt(pass, salt);
+            if (strcmp(argv[1], resultHash) == 0)
+            {
+                printf("password: %s, %ld iterations\n", pass, x);
+                printf("%s, %s\n", resultHash, argv[1]);
+                return 0;
+            }
+        }
+}
+}
+}
+    // iterate over all five digits
+        for (int i = 0; i < alphabet_len; i++)
+    {
+        pass[0] = alphabet[i];
+        x++;
+        for (int j = 0; j < alphabet_len; j++)
+        {
+            pass[1] = alphabet[j];
+            x++;
+        for (int k = 0; k < alphabet_len; k++)
+        {
+            pass[2] = alphabet[k];
+            x++;
+        for (int l = 0; l < alphabet_len; l++)
+        {
+            pass[3] = alphabet[l];
+            x++;
+        for (int m = 0; m < alphabet_len; m++)
+            pass[4] = alphabet[m];
+            x++;
+            resultHash = crypt(pass, salt);
+            if (strcmp(argv[1], resultHash) == 0)
+            {
+                printf("password: %s, %ld iterations\n", pass, x);
+                printf("%s, %s\n", resultHash, argv[1]);
+                return 0;
+            }
+        }
+}
+}
+}
 }
